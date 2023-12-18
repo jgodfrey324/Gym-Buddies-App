6e7d8313e79659635cd6da492817af7a70d5df8f
Jenna <jennagodfrey324@gmail.com>
2023-12-18 11:23:59 -0700

    updating to ts

--- expo-user-management/App.tsx
+++ expo-user-management/App.tsx
@@ -1,13 +1,13 @@
 import 'react-native-url-polyfill/auto'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Auth from './components/Auth'
+import { useState, useEffect } from 'react'
 import { supabase } from './lib/supabase'
+import Auth from './components/Auth'
+import Account from './components/Account'
+import { View } from 'react-native'
 import { Session } from '@supabase/supabase-js'
-import { useState } from 'react';
 
 export default function App() {
-  const [session, setSession] = useState(Session || null)
+  const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -23,14 +23,5 @@ export default function App() {
     <View>
       {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
