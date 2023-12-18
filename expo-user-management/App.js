9989d0d0c86ed5ca79db84ac07f5d7beeb995a89
Jenna <jennagodfrey324@gmail.com>
2023-12-18 11:17:39 -0700

    finishing app.js

--- expo-user-management/App.js
+++ expo-user-management/App.js
@@ -1,11 +1,27 @@
+import 'react-native-url-polyfill/auto'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth from './components/Auth'
+import { supabase } from './lib/supabase'
+import { Session } from '@supabase/supabase-js'
+import { useState } from 'react';
 
 export default function App() {
+  const [session, setSession] = useState(Session || null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
     </View>
   );
 }
