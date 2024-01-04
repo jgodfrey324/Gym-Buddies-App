58cc49e74425ce94ddf4fac382140157c80d9c4b
Jenna <jennagodfrey324@gmail.com>
2024-01-04 14:31:24 -0700

    working through buttons

--- expo-user-management/App.tsx
+++ expo-user-management/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="Auth" component={Auth} />
       </Stack.Navigator>
     </NavigationContainer>
--- expo-user-management/components/HomeScreen.tsx
+++ expo-user-management/components/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Alert, StyleSheet, View } from 'react-native'
+import { Alert, StyleSheet, Touchable, TouchableOpacity, View } from 'react-native'
 import { supabase } from '../lib/supabase'
 import { Button, Input, Text } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
@@ -10,22 +10,28 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.heading, styles.container]}>IronBonds</Text>
-      <Text style={styles.motto}>Motivation through community</Text>
+        <Text style={[styles.heading, styles.container]}>IronBonds</Text>
+        <Text style={styles.motto}>Motivation through community</Text>
 
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button style={styles.buttons} title="Sign in" disabled={loading} onPress={() => navigation.navigate('Auth')} />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button style={styles.buttons} title="Sign up" disabled={loading} onPress={() => navigation.navigate('Auth')} />
-      </View>
+        <View style={styles.buttonContainer}>
+            <View style={[styles.verticallySpaced, styles.mt20]}>
+            <TouchableOpacity style={styles.buttons} disabled={loading} onPress={() => navigation.navigate('Auth')} >
+                <Text style={styles.buttonsText} >Sign In</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={styles.verticallySpaced}>
+            <TouchableOpacity style={styles.buttons} disabled={loading} onPress={() => navigation.navigate('Auth')} >
+                <Text style={styles.buttonsText} >Sign Up</Text>
+            </TouchableOpacity>
+            </View>
+        </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 30,
+    marginTop: '18%',
     padding: 12,
   },
   verticallySpaced: {
@@ -49,9 +55,22 @@ const styles = StyleSheet.create({
     marginBottom: 40,
     marginLeft: 20
   },
+  buttonContainer: {
+    alignSelf: 'center',
+    bottom: '-75%',
+    width: '75%'
+  },
   buttons: {
-    marginTop: 5,
-    overflow: 'hidden',
-    borderRadius: 20
-  }
+    marginTop: 8,
+    // overflow: 'hidden',
+    borderRadius: 20,
+    width: '100%',
+    backgroundColor: '#242424',
+    height: 43,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonsText: {
+    color: 'white'
+  },
 })
