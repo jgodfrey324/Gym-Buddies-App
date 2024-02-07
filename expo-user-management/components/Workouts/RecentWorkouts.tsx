2f6cfd3ec448a0e991f68d9df94d15b268e5834c
Jenna <jennagodfrey324@gmail.com>
2024-02-06 17:13:26 -0700

    styling the recent workouts components

--- expo-user-management/components/Workouts/RecentWorkouts.tsx
+++ expo-user-management/components/Workouts/RecentWorkouts.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 export default function RecentWorkouts () {
 
     return (
         <View>
-            <Text>
-                This is where you view your recent workouts
+            <Text style={styles.header}>
+                Recent Workouts
             </Text>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 35,
+        textAlign: 'center',
+        fontWeight: '500',
+        padding: 20
+    }
+})
