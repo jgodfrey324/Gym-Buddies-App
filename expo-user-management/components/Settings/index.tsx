149c690637f5fbdabc8b361a4e12700f51c52a12
Raoul Andalis <raoulandalis1@yahoo.com>
2024-02-06 19:13:42 -0500

    made profile edit

--- expo-user-management/components/Settings/index.tsx
+++ expo-user-management/components/Settings/index.tsx
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { Alert, StyleSheet, View, TouchableOpacity, Text, TextInput, Platform, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { supabase } from '../../lib/supabase';
 import { useNavigation } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 export default function Settings () {
     return (
         <View style={styles.container}>
+            <View style={styles.profileEdit}>
+                <MaterialCommunityIcons
+                name='account-edit'
+                size={30}
+                />
+                <Text style={styles.profileText}>Profile</Text>
+            </View>
             <View>
                 <TouchableOpacity
                     style={styles.customButton}
@@ -34,11 +43,24 @@ const styles = StyleSheet.create({
         marginLeft: 'auto',
         marginRight: 'auto',
         width: '85%',
-        marginTop: '100%',
+        marginTop: 400,
         // marginBottom: 50
     },
     buttonText: {
         color: 'white',
         fontSize: 16,
     },
+    profileEdit: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        gap: 10,
+        top: 80
+    },
+    profileText: {
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        fontSize: 20
+    }
 })
