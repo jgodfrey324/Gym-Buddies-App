ef10f31189f2609c0ccfd2cae80836b475e46f7a
Jenna <jennagodfrey324@gmail.com>
2024-02-07 14:05:06 -0700

    setting up getting user's workouts function

--- expo-user-management/components/Workouts/RecentWorkouts.tsx
+++ expo-user-management/components/Workouts/RecentWorkouts.tsx
@@ -1,6 +1,44 @@
 import { View, Text, StyleSheet } from "react-native"
+import { useState } from "react"
 
 export default function RecentWorkouts () {
+    const [loading, setLoading] = useState(true)
+
+
+    async function getUserWorkouts() {
+        // try {
+        //     setLoading(true)
+        //     if (!session?.user) {
+        //     // console.log('there isn\'t a session user')
+        //         throw new Error('No user on the session!')
+        //     }
+
+        //     const { data, error, status } = await supabase
+        //     .from('profiles')
+        //     .select(`nickname, first_name, last_name, age, weight`)
+        //     .eq('id', session?.user.id)
+        //     .single()
+        //     if (error && status !== 406) {
+        //         console.log('error', error)
+        //         throw error
+        //     }
+
+        //     if (data) {
+        //         setNickname(data.nickname)
+        //         setFirstName(data.first_name)
+        //         setLastName(data.last_name)
+        //         setAge(data.age)
+        //         setWeight(data.weight)
+        //     }
+        // } catch (error) {
+        //     if (error instanceof Error) {
+        //         Alert.alert(error.message)
+        //     }
+        // } finally {
+        //     setLoading(false)
+        // }
+    }
+
 
     return (
         <View>
