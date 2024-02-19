49ea2183e938b9c6917b2e138e7e691c675ef907
Jenna <jennagodfrey324@gmail.com>
2024-02-19 16:30:05 -0700

    getting commit today

--- expo-user-management/components/Spinner/index.tsx
+++ expo-user-management/components/Spinner/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { Bounce } from 'react-native-animated-spinkit';
 
+console.log('this')
 const Spinner = () => (
   <View>
     <Bounce size={80} color="white" />
