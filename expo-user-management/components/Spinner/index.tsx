f3c6ec2aa414a885bec484f00590471e3d80da18
Jenna <jennagodfrey324@gmail.com>
2024-02-19 16:30:23 -0700

    its a design day

--- expo-user-management/components/Spinner/index.tsx
+++ expo-user-management/components/Spinner/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { View } from 'react-native';
 import { Bounce } from 'react-native-animated-spinkit';
 
-console.log('this')
 const Spinner = () => (
   <View>
     <Bounce size={80} color="white" />
